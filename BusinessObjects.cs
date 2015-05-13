952c919ae91fdfd504a8a35ccf86714cb92c86a0
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-13 15:19:54 +0200

    Added content.

---
+++ BusinessObjects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tx_BO_reporting
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+
+        private float fSalesTax = 1.07F;
+
+        public override string ToString()
+        {
+            return String.Format("The price of {0} ({1}) is including sales tax {2:c}", Name, ProductCode, Price * fSalesTax);
+        }
+    }
+
+    public class Report
+    {
+        public Product Product { get; set; }
+        public string Title { get; set; }
+    }
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tx_BO_reporting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mergeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Report> Reports = new List<Report>();
+
+            Product product = new Product();
+            product.Name = "TX Text Control .NET for Windows Forms Subscription";
+            product.Description = "1 developer license including all updates for 1 year incl. technical support";
+            product.Price = 1608.60F;
+            product.ProductCode = "TX-2000-DP-S";
+
+
+            Report report = new Report();
+            report.Title = "Sales Report";
+            report.Product = product;
+
+            Reports.Add(report);
+
+            mailMerge1.MergeObjects(Reports);
+        }
+    }
+}
